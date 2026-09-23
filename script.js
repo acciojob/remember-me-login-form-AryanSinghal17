874c2bc58fdc4bd1ac5d61994ffb8f83676e67ea
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-23 13:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let checkbox = document.getElementById("checkbox");
+let btn = document.getElementById("btn");
+let existing = document.getElementById("existing");
+
+
+let savedUsername = localStorage.getItem("username");
+
+if (savedUsername) {
+  existing.style.display = "block";
+}
+
+
+btn.addEventListener("click", () => {
+
+  if (!username.value || !password.value) {
+    alert("Please enter username and password");
+    return;
+  }
+
+  if (checkbox.checked) {
+    localStorage.setItem("username", username.value);
+    localStorage.setItem("password", password.value);
+
+    existing.style.display = "block";
+  }else{
+    localStorage.clear();
+  }
+
+  alert(`Logged in as ${username.value}`);
+});
+
+
+existing.addEventListener("click", () => {
+  let savedUsername = localStorage.getItem("username");
+
+  if (savedUsername) {
+    alert(`Logged in as ${savedUsername}`);
+  }
+});
